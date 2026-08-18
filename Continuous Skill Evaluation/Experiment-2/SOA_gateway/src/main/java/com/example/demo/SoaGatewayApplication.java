2865183a60e9b25a0b25d99920c2280eef8dfc84
vaishu <vaishnavi040806@gmail.com>
2026-08-18 22:05:44 +0530

    Add Experiment 2 API Gateway project

---
+++ Continuous Skill Evaluation/Experiment-2/SOA_gateway/src/main/java/com/example/demo/SoaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaGatewayApplication.class, args);
+    }
+
+}
